7b6a2f0f277cf8fbc945e406d6f4a3caa9846448
Joonas <jsuuronen9@gmail.com>
2024-11-24 21:37:01 +0200

    Add project files.

---
+++ CoreClasses/Participant.cs
@@ -0,0 +1,8 @@
+﻿
+public class Participant
+{
+	public string Name { get; set; }
+	public string? SelectedPoints { get; set; } // Null if no point selected
+	public bool IsScrumMaster { get; set; }
+}
+
---
+++ CoreClasses/PlanningService.cs
@@ -0,0 +1,63 @@
+﻿public class PlanningService
+{
+	public List<Participant> Participants { get; private set; } = new List<Participant>();
+	public bool PointsRevealed { get; private set; } = false;
+
+	// Event for notifying state changes
+	public event Action? OnStateChanged;
+
+	public void AddParticipant(string name, bool isScrumMaster = false)
+	{
+		if (!Participants.Any(p => p.Name == name))
+		{
+			isScrumMaster = isScrumMaster || !Participants.Any(p => p.IsScrumMaster);
+
+			Participants.Add(new Participant
+			{
+				Name = name,
+				IsScrumMaster = isScrumMaster,
+				SelectedPoints = null
+			});
+
+			RaiseStateChanged();
+		}
+	}
+
+	public void RevealPoints(string name)
+	{
+		if (Participants.Any(p => p.Name == name && p.IsScrumMaster))
+		{
+			PointsRevealed = true;
+			RaiseStateChanged();
+		}
+	}
+
+	public void Reset(string name)
+	{
+		if (Participants.Any(p => p.Name == name && p.IsScrumMaster))
+		{
+			foreach (var participant in Participants)
+			{
+				participant.SelectedPoints = null;
+			}
+			PointsRevealed = false;
+			RaiseStateChanged();
+		}
+	}
+
+	public void UpdateParticipantPoints(string name, string? points)
+	{
+		var participant = Participants.FirstOrDefault(p => p.Name == name);
+		if (participant != null)
+		{
+			participant.SelectedPoints = points;
+			RaiseStateChanged();
+		}
+	}
+
+	// Private method to safely invoke the event
+	private void RaiseStateChanged()
+	{
+		OnStateChanged?.Invoke();
+	}
+}
